142fafe0a77cc733b81aeb8af096c9dee2d2073d
MaksymMV <maslakovmaksym243@gmail.com>
2025-02-14 19:29:45 +0200

    change select styles

--- src/plugins/select-categories/admin/src/components/SelectCategories/StyledComponents.tsx
+++ src/plugins/select-categories/admin/src/components/SelectCategories/StyledComponents.tsx
@@ -1,5 +1,16 @@
-import { Checkbox } from '@strapi/design-system';
+import { TextInput } from '@strapi/design-system';
 import styled from 'styled-components';
+
+export const InputWrapper = styled.div`
+  padding: 0.8rem 0 0 2rem;
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+  background-color: #fff;
+`;
+
+export const Input = styled(TextInput)``;
 
 export const Wrapper = styled.div`
   position: relative;
@@ -14,27 +25,66 @@ export const Menu = styled.div`
   background: white;
   border: 0.1rem solid #ccc;
   border-radius: 0.4rem;
-  max-height: 30rem;
+  max-height: 40rem;
   overflow-y: auto;
+  padding-right: 2rem;
   z-index: 1000;
-  padding: 0.8rem 0;
 `;
 
-export const Option = styled.div`
-  padding: 0.4rem 0.8rem;
+export const Option = styled.div<{ isFirst: boolean }>`
+  padding: 0.4rem 0 0 ${({ isFirst }) => (isFirst ? '2' : '3')}rem;
 `;
 
-export const OptionInner = styled.div<{ checked: boolean }>`
+export const OptionInner = styled.div<{ checked: boolean; selectable: boolean }>`
   padding: 0rem 1rem;
   border-radius: 0.4rem;
-  background-color: ${({ checked }) => (checked ? 'rgba(72, 69, 255, 0.24)' : 'transparent')};
+  background-color: ${({ checked, selectable }) =>
+    checked && selectable ? 'rgba(72, 69, 255, 0.24)' : 'transparent'};
+  border: ${({ selectable }) => (!selectable ? '1px solid #dcdce4' : 'none')};
 
   &:hover {
-    background-color: rgba(72, 69, 255, 0.24);
+    background-color: ${({ selectable }) =>
+      selectable ? 'rgba(72, 69, 255, 0.24)' : 'rgba(220, 220, 228, 0.4)'};
   }
 
   & > div > label {
     min-width: 100%;
     padding: 0.8rem 0;
   }
+`;
+
+export const Select = styled.div`
+  width: 100%;
+  font-size: 1.4rem;
+  border: 1px solid #dcdce4;
+  border-radius: 0.4rem;
+  padding: 0.8rem 1.6rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const SelectedCategoriesMenu = styled.div`
+  width: 100%;
+  background: white;
+  border: 0.1rem solid #ccc;
+  border-radius: 0.4rem;
+  max-height: 40rem;
+  overflow-y: auto;
+  padding: 0.8rem 2rem 0.8rem 0;
+  margin-top: 1rem;
+`;
+
+export const SelectedCategory = styled.div`
+  padding: 0.8rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1.4rem;
+`;
+
+export const Chevron = styled.div<{ isOpen: boolean }>`
+  transition: all 300ms ease;
+  transform: rotate(${({ isOpen }) => (isOpen ? '180' : '0')}deg);
 `;
